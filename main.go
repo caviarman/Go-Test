5b12b5aa7280c9ca4610e308335f400ddd7a4de2
Vladimir Ilin <mark09072010@gmail.com>
2019-05-30 22:33:42 +0300

    route handles added

--- main.go
+++ main.go
@@ -1,26 +1,106 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
-	"os"
+	"strconv"
+	"time"
 
-	"github.com/russross/blackfriday"
+	"github.com/gorilla/mux"
 )
 
-func main() {
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	http.HandleFunc("/markdown", GenerateMarkdown)
-	http.Handle("/", http.FileServer(http.Dir("public")))
-	http.ListenAndServe(":"+port, nil)
+//Note ...
+type Note struct {
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CreatedOn   time.Time `json:"createdOn"`
 }
 
-// GenerateMarkdown ...
-func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
-	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
-	rw.Header().Add("Content-Type", "text/html")
-	rw.Write(markdown)
+//Store for the Notes collection
+var noteStore = make(map[string]Note)
+
+//Variable to generate key for the collection
+var id int
+
+//PostNoteHandler ...
+func PostNoteHandler(w http.ResponseWriter, r *http.Request) {
+	var note Note
+
+	err := json.NewDecoder(r.Body).Decode(&note)
+	if err != nil {
+		panic(err)
+	}
+	note.CreatedOn = time.Now()
+	id++
+	k := strconv.Itoa(id)
+	noteStore[k] = note
+
+	j, err := json.Marshal(note)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(j)
+}
+
+//GetNoteHandler ...
+func GetNoteHandler(w http.ResponseWriter, r *http.Request) {
+	var notes []Note
+	for _, v := range noteStore {
+		notes = append(notes, v)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	j, err := json.Marshal(notes)
+	if err != nil {
+		panic(err)
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(j)
+}
+
+//PutNoteHandler ...
+func PutNoteHandler(w http.ResponseWriter, r *http.Request) {
+	var err error
+	vars := mux.Vars(r)
+	k := vars["id"]
+	var noteToUpd Note
+	err = json.NewDecoder(r.Body).Decode(&noteToUpd)
+	if err != nil {
+		panic(err)
+	}
+	if note, ok := noteStore[k]; ok {
+		noteToUpd.CreatedOn = note.CreatedOn
+		delete(noteStore, k)
+		noteStore[k] = noteToUpd
+	} else {
+		log.Printf("Could not find key of Note %s to delete", k)
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+//DeleteNoteHandler ...
+func DeleteNoteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	k := vars["id"]
+	if _, ok := noteStore[k]; ok {
+		delete(noteStore, k)
+	} else {
+		log.Printf("Could not find key of Note %s to delete", k)
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+func main() {
+	r := mux.NewRouter().StrictSlash(false)
+	r.HandleFunc("/api/notes", GetNoteHandler).Methods("GET")
+	r.HandleFunc("/api/notes", PostNoteHandler).Methods("POST")
+	r.HandleFunc("/api/noted/{id}", PutNoteHandler).Methods("PUT")
+	r.HandleFunc("/api/notes/{id}", DeleteNoteHandler).Methods("DELETE")
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+	log.Println("Listening...")
+	server.ListenAndServe()
 }
